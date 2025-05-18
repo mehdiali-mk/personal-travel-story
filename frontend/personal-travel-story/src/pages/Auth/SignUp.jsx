627dd0f3357ad018207b092603402ee86029528e
Mehdiali Kadiwala <mehdialikadiwala@gmail.com>
2025-05-18 17:04:10 +0530

    SignUp page with functionality completed.

--- frontend/personal-travel-story/src/pages/Auth/SignUp.jsx
+++ frontend/personal-travel-story/src/pages/Auth/SignUp.jsx
@@ -1,5 +1,141 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import PasswordInput from "../../components/Input/PasswordInput.jsx";
+import { validateEmail } from "../../utils/helper.js";
+import axiosInstance from "../../utils/axiosInstance.js";
 
 export default function SignUp() {
-  return <div></div>;
+  const navigate = useNavigate();
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  async function handleSignup(event) {
+    event.preventDefault();
+
+    if (!name) {
+      setError("Please enter your name.");
+      return;
+    }
+
+    if (!validateEmail(email)) {
+      setError("Please enter a valid email address.");
+      return;
+    }
+
+    if (!password) {
+      setError("Please enter the password.");
+      return;
+    }
+
+    setError(null);
+
+    // Signup Call Backend API.
+    try {
+      const response = await axiosInstance.post("/create-account", {
+        fullName: name,
+        email: email,
+        password: password,
+      });
+
+      if (response.data && response.data.accessToken) {
+        localStorage.setItem("token", response.data.accessToken);
+        navigate("/dashboard");
+      }
+    } catch (error) {
+      if (
+        error.response &&
+        error.response.data &&
+        error.response.data.message
+      ) {
+        setError(error.response.data.message);
+      } else {
+        setError("An unexpected error occurred. Please try again.");
+      }
+    }
+  }
+
+  return (
+    <div className="h-screen bg-cyan-100 overflow-hidden relative">
+      <div className="login-ui-box right-10 -top-40" />
+      <div className="login-ui-box bg-cyan-200 -bottom-40 right-1/2" />
+
+      <div className="container h-screen flex items-center justify-center px-20 mx-auto">
+        <div className="w-2/5 h-[90vh] flex items-start signup-bg-img-1 bg-cover bg-center rounded-lg p-10 z-50">
+          <div>
+            <h4 className="text-5xl text-white font-semibold leading-[58px]">
+              Join The <br /> Adventure
+            </h4>
+            <p className="text-[15px] text-white leading-6 pr-7 mt-4">
+              Create an account to start documenting your travels and preserving
+              your memories in your personal travel journal.
+            </p>
+          </div>
+        </div>
+
+        <div className="w-2/5 h-[80vh] bg-white rounded-r-lg relative p-16 shadow-lg shadow-cyan-200/20">
+          <form
+            action=""
+            onSubmit={(e) => {
+              handleSignup(e);
+            }}
+          >
+            <h4 className="text-2xl font-semibold mb-7">SignUp</h4>
+
+            <input
+              type="text"
+              name="name"
+              id="text"
+              className="input-box"
+              placeholder="Full Name"
+              value={name}
+              onChange={({ target }) => {
+                setName(target.value);
+              }}
+            />
+            <input
+              type="text"
+              name="email"
+              id="email"
+              className="input-box"
+              placeholder="Email"
+              value={email}
+              onChange={({ target }) => {
+                setEmail(target.value);
+              }}
+            />
+
+            <PasswordInput
+              value={password}
+              onChange={({ target }) => {
+                setPassword(target.value);
+              }}
+            />
+
+            {error && <p className="text-red-500 text-xs pb-1">{error}</p>}
+
+            <button type="submit" className="btn-primary">
+              CREATE ACCOUNT
+            </button>
+
+            <p className="text-xs text-slate-500 text-center my-4">Or</p>
+
+            <button
+              type="button"
+              className="btn-primary btn-light"
+              onClick={(event) => {
+                event.preventDefault();
+                navigate("/login");
+              }}
+            >
+              {" "}
+              LOGIN
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
